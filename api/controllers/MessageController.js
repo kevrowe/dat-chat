477d8e7264d416f1952b0a230939dc72ad561e8d
Kev Rowe <kev.rowe@gmail.com>
2015-06-25 17:28:04 +0100

    added destroyMany action for Message

--- api/controllers/MessageController.js
+++ api/controllers/MessageController.js
@@ -6,6 +6,26 @@
  */
 
 module.exports = {
-	
+	destroyMany: function (req, res) {
+		Message.find({'where': { 'chatId' : {'equals': req.query.chatId } }}).exec(function(err, messages) {
+			if (err) {
+				return res.serverError(err);
+			}
+
+			// Apparently if Model.find doesn't find anything matching the
+			// "where" clause, it just returns everything... helpful.
+			if (messages.length === 0 || messages[0].chatId !== req.query.chatId) {
+				return res.json({ status: 200 });
+			}
+
+			var messageIds = messages.map(function(e, i) {
+				return e.id;
+			});
+
+			Message.destroy({ id: messageIds }).exec(function(err, deletedMessages) {});
+
+			return res.json({ status: 200 });
+		});
+	}
 };
 
